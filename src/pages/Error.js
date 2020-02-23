7cc73825392b6b00cc370d2427b2f3dc2c0285cb
Łukasz <riraseth@gmail.com>
2020-02-23 14:27:13 +0100

    banner

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
 const Error = () => {
-  return <Hero></Hero>;
+  return (
+    <Hero>
+      <Banner title="404" subtitle="Page not found">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Error;
