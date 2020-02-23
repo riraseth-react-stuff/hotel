bd21ea357e28f67ecc4de6448a91c7522a05e239
Łukasz <riraseth@gmail.com>
2020-02-23 14:01:04 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import Error from './pages/Error';
 
 import { Route, Switch } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <React.Fragment>
+      <Navbar></Navbar>
       <Switch>
         <Route exact path="/" component={Home}></Route>
         <Route exact path="/rooms/" component={Rooms}></Route>
