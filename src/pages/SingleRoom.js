f2e5c55a9f593e9617ac6adebb9ea5950efa234e
Łukasz <riraseth@gmail.com>
2020-02-23 22:22:53 +0100

    Update SingleRoom.js

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -37,14 +37,49 @@ export default class SingleRoom extends Component {
       pets,
       images
     } = room;
+    const [mainImg, ...defaultImg] = images; // arry destructuring rest operator
     return (
-      <StyledHero img={images[0] || this.state.defaultBcg}>
-        <Banner title={name}>
-          <Link to="/rooms" className="btn-primary">
-            back to rooms
-          </Link>
-        </Banner>
-      </StyledHero>
+      <React.Fragment>
+        <StyledHero img={mainImg || this.state.defaultBcg}>
+          <Banner title={name}>
+            <Link to="/rooms" className="btn-primary">
+              back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {defaultImg.map((image, index) => {
+              return <img key={index} src={image} alt={name}></img>;
+            })}
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price: ${price}</h6>
+              <h6>size: {size} SQFT</h6>
+              <h6>
+                max capacity:{' '}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
+              <h6>{breakfast && 'free breakfast included'}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((extra, index) => {
+              return <li key={index}>- {extra}</li>;
+            })}
+          </ul>
+        </section>
+      </React.Fragment>
     );
   }
 }
