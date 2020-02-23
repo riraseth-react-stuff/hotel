67d6eb549573573985b33cd7692bf136b712ab07
Łukasz <riraseth@gmail.com>
2020-02-23 19:46:44 +0100

    context

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { RoomContext } from '../context';
+import Loading from './Loading';
+import Room from './Room';
+export default class FeaturedRooms extends Component {
+  static contextType = RoomContext;
+  render() {
+    const { featuredRooms: rooms } = this.context;
+    console.log(rooms);
+    return (
+      <div>
+        from featured
+        <Room></Room>
+        <Loading></Loading>
+      </div>
+    );
+  }
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import Services from '../components/Services';
+import FeaturedRooms from '../components/FeaturedRooms';
 import { Link } from 'react-router-dom';
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
         </Banner>
       </Hero>
       <Services></Services>
+      <FeaturedRooms></FeaturedRooms>
     </React.Fragment>
   );
 };
