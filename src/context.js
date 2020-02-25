ab51f967056d3c18466b275835ffb556ad166d8f
Łukasz <riraseth@gmail.com>
2020-02-25 21:18:34 +0100

    prices

--- src/context.js
+++ src/context.js
@@ -87,7 +87,7 @@ class RoomProvider extends Component {
     let tempRooms = [...rooms];
     // transform values
     capacity = parseInt(capacity);
-
+    price = parseInt(price);
     //filter by type
     if (type !== 'all') {
       tempRooms = tempRooms.filter(room => room.type === type);
@@ -98,6 +98,11 @@ class RoomProvider extends Component {
     if (capacity !== 1) {
       tempRooms = tempRooms.filter(room => room.capacity === capacity);
     }
+
+    // filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price);
+
+    //change state
     this.setState({
       sortedRooms: tempRooms
     });
